ea3874c05a24a2c0a6dff16cb0c2e5823c2990ca
Sarthakvrraut <sarthakvrraut@gmail.com>
2025-09-21 01:22:47 +0530

    v

--- run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-# Name of your Streamlit script
-script_name = "omr_streamlit_app.py"
-
-# Run Streamlit
-subprocess.run([sys.executable, "-m", "streamlit", "run", script_name])
-
